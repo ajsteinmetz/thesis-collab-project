0f8553f30200c31342f6d51d3eb866e3515e6e6f
Andrew <ajsteinmetz@email.arizona.edu>
2024-09-26 01:37:52 +0800

    Removed excess files.

---
+++ extra_fig_removal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for image_file in $(ls plots/)
+do
+if grep $image_file *.log -c > 1
+then
+        echo "File $image_file is in use."
+else
+        echo "File $image_file is not in use."
+        mv "plots/$image_file" "plots/moved.$image_file" # or any other action
+fi
+done
